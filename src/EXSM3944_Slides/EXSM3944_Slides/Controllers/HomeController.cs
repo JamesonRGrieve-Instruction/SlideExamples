20400be3010396816b998fec590a3d34bf3d67af
James Grieve <jgrieve@ualberta.ca>
2023-03-09 10:06:38 -0700

    Project initialization;

---
+++ src/EXSM3944_Slides/EXSM3944_Slides/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EXSM3944_Slides.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EXSM3944_Slides.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/EXSM3944_Slides/EXSM3944_Slides/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EXSM3944_Slides.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
