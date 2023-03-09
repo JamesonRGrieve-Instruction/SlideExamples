8d4c1ae042d5c2e1821b0a473a6e0c5e854f3e34
James Grieve <jgrieve@ualberta.ca>
2023-03-09 12:08:27 -0700

    First checkpoint; Authentication set up, register and login work;

---
+++ src/EXSM3944_Slides/EXSM3944_Slides/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EXSM3944_Slides.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
