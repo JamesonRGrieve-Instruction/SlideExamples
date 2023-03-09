de6a7aa34fed80b3fb431bd8a14bd1bdf1956ccc
James Grieve <jgrieve@ualberta.ca>
2023-03-09 10:19:04 -0700

    First checkpoint; Model, views and controller initialized;

---
+++ src/EXSM3944_Slides/EXSM3944_Slides/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace EXSM3944_Slides.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+    }
+}
+
